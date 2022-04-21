56b20e84eae5b5268b1b47308289f7417d3f944f
Otto <gonott@gmail.com>
2022-04-21 01:58:39 -0300

    ..

--- frontend/PersonalizedVue.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-
-
-var vm = new Vue({
-    el: '#div1',
-    data: { toDoName: "Un Nombre" }
-})
-
---
+++ frontend/mainvue.js
@@ -0,0 +1,39 @@
+
+const editDiv= 
+`<b>Editing task {{ editToDoName }}</b></br></br>
+<input type="text" name="editTxtBx" value="New Name">
+<input type="button" name="EditBtn" value="Edit">
+`
+
+var myitems = [
+{name: "ItemA", done: 1},
+{name: "ItemB", done: 1},
+{name: "itemC", done: 0}, 
+{name: "itemD", done: 0}
+]
+
+var thisitem = "algo";
+var editToDoName = "ItemItem";
+
+var mainDiv = new Vue({
+el: '#maindiv',
+data: { items: myitems},
+methods: {
+    editItem(item){
+        myitems = myitems.filter((obj)=> {
+            thisitem = item;
+            })
+        }  
+    changeDiv(item.name){
+    this.el = "#mainHTML",
+    this.data= {editToDoName: thisitem}  }
+    
+    }
+    
+
+})
+
+
+
+        
+      
